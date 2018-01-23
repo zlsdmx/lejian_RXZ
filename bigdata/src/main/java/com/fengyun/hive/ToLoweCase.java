6ae1c43d5d01c71f259be5c01bd0b232b57edd84
zlsdmx <zheng_shengsheng@163.com>
2018-01-23 11:20:48 +0800

    添加自定义hive函数的注解

--- bigdata/src/main/java/com/fengyun/hive/ToLoweCase.java
+++ bigdata/src/main/java/com/fengyun/hive/ToLoweCase.java
@@ -6,7 +6,14 @@ import org.apache.hadoop.io.Text;
 /**
  * 自定义hive函数的写法。
  * @author zhengss
- *
+ *1、先开发一个java类，继承UDF，并重载evaluate方法
+ *2、打成jar包上传到服务器
+3、将jar包添加到hive的classpath
+hive>add JAR /home/hadoop/udf.jar;
+4、创建临时函数与开发好的java class关联
+Hive>create temporary function tolowercase as 'cn.itcast.bigdata.udf.ToProvince';
+
+5、即可在hql中使用自定义的函数strip 
  */
 
 public class ToLoweCase extends UDF {
